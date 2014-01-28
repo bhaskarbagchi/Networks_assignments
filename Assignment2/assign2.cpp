0c9a6600794f74f76f460451d92834513e44c274
Bhaskar Bagchi <bhaskarbagchi13101992@gmail.com>
2014-01-29 00:40:15 +0530

    Part 1 completed for 1 sample case of shifting

--- Assignment2/assign2.cpp
+++ Assignment2/assign2.cpp
@@ -14,71 +14,136 @@
 
 //Definitions
 #define NO_OF_SINUSOIDS 5
-#define NO_OF_SAMPLING_POINTS 16384
+#define NO_OF_SAMPLING_POINTS 524288                            //=No of bins
+#define SAMPLING_TIME 0.000000002                               //(1/500000000)
+                                                                //Therefore freq of each bin = (Sampling freq / 2) / (No of bins / 2) = 953.67 Hz
+                                                                //Therefore total frequency in the band = 953.67 * 524288 = 500000000
+                                                                //Total sampling time = 0.000000002*524288 = 0.001048 secs > (1/3500) time period of wave with leasat frequency
 #define A_MAX 500
 #define PI 3.14159265358979323846
 
 using namespace std;
 
 //Function Prototypes
-void generateWaveform(double fFrom, double* freq, double* amp, double* val, double* time);
+void generateWaveform(double* freq, double* amp, double* val, double* time);
 void FFT(double* data, int size, complex<double>* fft);
 void IFFT(complex<double>* data, int size, complex<double>* fft);
+void shiftAndMerges(complex<double>* DFT1, int shift1, complex<double>* DFT2, int shift2, complex<double>* shiftedFFT);
+void retriveWaves(complex<double>* shiftedFFT, int shift1, complex<double>* FirstFFT, int shift2, complex<double>* SecondFFT);
+double rmsError(double* wave1, double* wave2);
 
 //Main
 int main(int arcg, char* argv[]){
-  
-  double *ampWave1, *ampWave2, *freqWave1, *freqWave2;
-  double *valWave1, *valWave2;
-  double *timeWave1, *timeWave2;
-  
-  complex<double>* DFT1 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
-  complex<double>* IDFT1 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
-  
-  ampWave1 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
-  ampWave2 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
-  
-  freqWave1 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
-  freqWave2 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
-  
-  valWave1 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
-  valWave2 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
-  
-  timeWave1 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
-  timeWave2 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
-  
-  // First band from 6kHz to 10 kHz
-  generateWaveform(6.0, freqWave1, ampWave1, valWave1, timeWave1);
-  // Second band from 15 kHz to 19 kHz
-  generateWaveform(15.0, freqWave2, ampWave2, valWave2, timeWave2);
-  cout<<"Waves generated"<<endl;
-  
-  FFT(valWave1, NO_OF_SAMPLING_POINTS, DFT1);
-  cout<<"FFT done"<<endl;
-  
-  IFFT(DFT1, NO_OF_SAMPLING_POINTS, IDFT1);
-  cout<<"IFFT done"<<endl;
-  
-  for(int i = 0; i<NO_OF_SAMPLING_POINTS; i++){
-          cout<<valWave1[i]<<"          "<<real(IDFT1[i])/NO_OF_SAMPLING_POINTS<<endl;
-  }
-  cout<<"Printed"<<endl;
-  
-  //free ampWave, freqWave, valWave and timeWave
-  free(ampWave1);
-  free(freqWave1);
-  free(ampWave2);
-  free(freqWave2);
-  free(valWave1);
-  free(timeWave1);
-  free(valWave2);
-  free(timeWave2);
-  return 0;
+
+        double *ampWave1, *ampWave2, *freqWave1, *freqWave2;
+        double *valWave1, *valWave2;
+        double *timeWave1, *timeWave2;
+
+        complex<double>* DFT1 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+        complex<double>* IDFT1 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+        complex<double>* DFT2 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+        complex<double>* IDFT2 = (complex<double>*)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+
+        ampWave1 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
+        ampWave2 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
+
+        freqWave1 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
+        freqWave2 = (double *)malloc(NO_OF_SINUSOIDS*sizeof(double));
+
+        valWave1 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
+        valWave2 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
+
+        timeWave1 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
+        timeWave2 = (double *)malloc(NO_OF_SAMPLING_POINTS*sizeof(double));
+
+        generateWaveform(freqWave1, ampWave1, valWave1, timeWave1);
+        generateWaveform(freqWave2, ampWave2, valWave2, timeWave2);
+        cout<<"Waves generated"<<endl;
+
+        FFT(valWave1, NO_OF_SAMPLING_POINTS, DFT1);
+        cout<<"First FFT done"<<endl;
+
+        IFFT(DFT1, NO_OF_SAMPLING_POINTS, IDFT1);
+        cout<<"First IFFT done"<<endl;
+
+        FFT(valWave2, NO_OF_SAMPLING_POINTS, DFT2);
+        cout<<"Second FFT done"<<endl;
+
+        IFFT(DFT2, NO_OF_SAMPLING_POINTS, IDFT2);
+        cout<<"Second IFFT done"<<endl;
+
+        //for(int i = (NO_OF_SAMPLING_POINTS/10); i<NO_OF_SAMPLING_POINTS; i++){
+                //for(int j = (NO_OF_SAMPLING_POINTS/10); j<NO_OF_SAMPLING_POINTS; j++){
+                        int i, j, shift1, shift2;
+                        cout<<"Enter shift for 1st wave in Hz : ";
+                        cin>>shift1;
+                        cout<<"Enter shift for 2nd wave in Hz : ";
+                        cin>>shift2;
+                        i = shift1/953.67;
+                        j = shift2/953.67;
+                        
+                        //Transmitter
+                        complex<double>* shiftedFFT = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        complex<double>* shiftedIFFT = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        shiftAndMerges(DFT1, i, DFT2, j, shiftedFFT);
+                        IFFT(shiftedFFT, NO_OF_SAMPLING_POINTS, shiftedIFFT);
+                        
+                        //Reciever
+                        complex<double>* shiftedFFTreciever = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        complex<double>* recievedFirstFFT = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        complex<double>* recievedSecondFFT = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        complex<double>* recievedFirstCmplx = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        complex<double>* recievedSecondCmplx = (complex<double> *)malloc(NO_OF_SAMPLING_POINTS * sizeof(complex<double>));
+                        double* recieved = (double *)malloc(NO_OF_SAMPLING_POINTS * sizeof(double));
+                        double* recievedFirst = (double *)malloc(NO_OF_SAMPLING_POINTS * sizeof(double));
+                        double* recievedSecond = (double *)malloc(NO_OF_SAMPLING_POINTS * sizeof(double));
+                        for(int k = 0; k<NO_OF_SAMPLING_POINTS; k++){
+                                recieved[k] = real(shiftedIFFT[i]);
+                        }
+                        FFT(recieved, NO_OF_SAMPLING_POINTS, shiftedFFTreciever);
+                        retriveWaves(shiftedFFTreciever, i, recievedFirstFFT, j, recievedSecondFFT);
+                        IFFT(recievedFirstFFT, NO_OF_SAMPLING_POINTS, recievedFirstCmplx);
+                        IFFT(recievedSecondFFT, NO_OF_SAMPLING_POINTS, recievedSecondCmplx);
+                        //collect only the real part
+                        for(int k = 0; k<NO_OF_SAMPLING_POINTS; k++){
+                                recievedFirst[k] = real(recievedFirstCmplx[i]);
+                                recievedSecond[k] = real(recievedSecondCmplx[i]);
+                        }
+                        //compare with real waves
+                        double error1 = rmsError(valWave1, recievedFirst);
+                        double error2 = rmsError(valWave2, recievedSecond);
+                        cout<<"The root mean squared errors are as follows:"<<endl;
+                        cout<<"\t\tWave1 = "<<error1<<endl;
+                        cout<<"\t\tWave2 = "<<error2<<endl;
+                        
+                        free(shiftedFFT);
+                        free(shiftedIFFT);
+                        free(shiftedFFTreciever);
+                        free(recievedFirstFFT);
+                        free(recievedSecondFFT);
+                        free(recievedFirstCmplx);
+                        free(recievedSecondCmplx);
+                        free(recieved);
+                        free(recievedFirst);
+                        free(recievedSecond);
+                //}
+        //}
+
+        //free ampWave, freqWave, valWave and timeWave
+        free(ampWave1);
+        free(freqWave1);
+        free(ampWave2);
+        free(freqWave2);
+        free(valWave1);
+        free(timeWave1);
+        free(valWave2);
+        free(timeWave2);
+        return 0;
 }
 
 //Function definitions
 
-void generateWaveform(double fFrom, double* freq, double* amp, double* val, double* time){
+void generateWaveform(double* freq, double* amp, double* val, double* time){
         int i, j;
         
         double **t, **F;
@@ -86,7 +151,7 @@ void generateWaveform(double fFrom, double* freq, double* amp, double* val, doub
         F = (double **)malloc(NO_OF_SINUSOIDS * sizeof(double*));
         
         for (i = 0; i < NO_OF_SINUSOIDS; i++)
-        {       freq[i] = fFrom + 4.0 * ((double)rand() / (RAND_MAX));
+        {       freq[i] = 3.50 + (20.0 - 3.50) * ((double)rand() / (RAND_MAX));
                 amp[i] = ( A_MAX / NO_OF_SINUSOIDS ) * ((double)rand() / (RAND_MAX));
                 t[i] = (double *)malloc(NO_OF_SAMPLING_POINTS * sizeof(double));
                 F[i] = (double *)malloc(NO_OF_SAMPLING_POINTS * sizeof(double));
@@ -94,13 +159,13 @@ void generateWaveform(double fFrom, double* freq, double* amp, double* val, doub
         
         for (i = 0; i < NO_OF_SINUSOIDS; i++)
         {       for (j = 0; j < NO_OF_SAMPLING_POINTS; ++j)
-                {       t[i][j] = (double) j * 0.00006;
+                {       t[i][j] = (double) j * SAMPLING_TIME;
                         F[i][j] = amp[i] * sinf(2.0 * PI * freq[i] * t[i][j]);
                 }
         }
         
         for (j = 0; j < NO_OF_SAMPLING_POINTS; ++j)
-        {       time[j] = (double) j * 0.00006;
+        {       time[j] = (double) j * SAMPLING_TIME;
                 val[j] = F[0][j] + F[1][j] + F[2][j] + F[3][j] + F[4][j];
         }
         
@@ -176,4 +241,90 @@ void IFFT(complex<double>* data, int size, complex<double>* ifft){
         free(a0);
         free(a1);
         return;
+}
+
+void shiftAndMerges(complex<double>* DFT1, int shift1, complex<double>* DFT2, int shift2, complex<double>* shiftedFFT){
+        int min = (shift1 < shift2)? shift1: shift2;
+        int max = (shift1 > shift2)? shift1: shift2;
+        int i = 0, j = 0, k = 0;
+        for(i = 0; i<min; i++){
+                complex<double> temp(0.0, 0.0);
+                shiftedFFT[i] = temp;
+        }
+        if(shift1 < shift2){
+                for(; i<max; i++){
+                        shiftedFFT[i] = DFT1[j];
+                        j++;
+                }
+                for(; i< NO_OF_SAMPLING_POINTS; i++){
+                        shiftedFFT[i] = /*DFT1[j] +*/ DFT2[k];
+                        //j++;
+                        k++;
+                }
+                return;
+        }
+        else{
+                for(; i<max; i++){
+                        shiftedFFT[i] = DFT2[k];
+                        k++;
+                }
+                for(; i< NO_OF_SAMPLING_POINTS; i++){
+                        shiftedFFT[i] = DFT1[j];// + DFT2[k];
+                        j++;// k++;
+                }
+                return;
+        }
+}
+
+void retriveWaves(complex<double>* shiftedFFT, int shift1, complex<double>* FirstFFT, int shift2, complex<double>* SecondFFT){
+        int min = (shift1 < shift2)? shift1: shift2;
+        int max = (shift1 > shift2)? shift1: shift2;
+        int i = 0, j = 0, k = 0;
+        if(shift1<shift2){
+                for(i = shift1; i<shift2; i++){
+                        FirstFFT[j] = shiftedFFT[i];
+                        j++;
+                }
+                for(; i<NO_OF_SAMPLING_POINTS; i++){
+                        SecondFFT[k] = shiftedFFT[i];
+                        k++;
+                }
+                for(; j<NO_OF_SAMPLING_POINTS; j++){
+                        complex<double> temp(0.0, 0.0);
+                        FirstFFT[j] = temp;
+                }
+                for(; k<NO_OF_SAMPLING_POINTS; k++){
+                        complex<double> temp(0.0, 0.0);
+                        SecondFFT[k] = temp;
+                }
+                return;
+        }
+        else{
+                for(i = shift2; i<shift1; i++){
+                        SecondFFT[k] = shiftedFFT[i];
+                        k++;
+                }
+                for(; i<NO_OF_SAMPLING_POINTS; i++);{
+                        FirstFFT[j] = shiftedFFT[i];
+                        j++;
+                }
+                for(; j<NO_OF_SAMPLING_POINTS; j++){
+                        complex<double> temp(0.0, 0.0);
+                        FirstFFT[j] = temp;
+                }
+                for(; k<NO_OF_SAMPLING_POINTS; k++){
+                        complex<double> temp(0.0, 0.0);
+                        SecondFFT[k] = temp;
+                }
+                return;
+        }
+}
+
+double rmsError(double* wave1, double* wave2){
+        double e = 0.0;
+        for(int i = 0; i<NO_OF_SAMPLING_POINTS; i++){
+                e += pow((wave1[i] - wave2[i]),2);
+        }
+        e /= (double)NO_OF_SAMPLING_POINTS;
+        return sqrt(e);
 }
